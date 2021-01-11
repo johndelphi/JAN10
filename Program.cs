c84d2478775100afa6b347e3622e0bf22d118ec2
jomo4jo@hotmail.com <jomo4jo@hotmail.com>
2021-01-10 19:11:42 -0600

    deleted console writeline method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.
+namespace JAN10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
